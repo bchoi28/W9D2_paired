0ff2f1f3f888b456c96f1024d262dba4446c01c0
Jiong Qi <jiongqipan@gmail.com>
2023-01-10 17:59:28 -0500

    create grid almost done?

--- Browser_TicTacToe/src/ttt-view.js
+++ Browser_TicTacToe/src/ttt-view.js
@@ -1,11 +1,23 @@
 class View {
   constructor(game, el) {
     this.game = game;
-
+    this.el = el;
+    this.setupBoard();
   }
-  setupBoard() { }
-  createElement
-  append
+  setupBoard() {
+    const ul = document.createElement("ul");
+
+    for (let row = 0; row < 3; row++) {
+      for (let col = 0; col < 3; col++) {
+        const li = document.createElement("li");
+        li.dataset.pos = JSON.stringify([row, col]);
+        ul.append(li);
+      }
+    }
+
+    this.el.append(ul);
+  }
+
   bindEvents() { }
 
   handleClick(e) { }
